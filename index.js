409feb4d6a9a161841c65b764da32b40b1e127e6
mrchristopher4 <155497916+mrchristopher4@users.noreply.github.com>
2024-03-19 19:50:50 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const selectBtnEl = document.getElementById("selectBtn");
+
+const colorSelectorEl = document.getElementById("colorSelector");
+
+
+selectBtnEl.addEventListener('submit', function (event) {
+    
+})
+
+/*
+fetch("https://www.thecolorapi.com/id{?hex,rgb,hsl,cmyk,format}", {
+    method: "GET",
+    headers: {"Content-Type":"application/json"}
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+*/
