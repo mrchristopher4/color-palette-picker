8dcd0d796c2d85ddcd5a4e8c1d2d6eb3642adc55
mrchristopher4 <155497916+mrchristopher4@users.noreply.github.com>
2024-03-20 22:24:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-const selectBtnEl = document.getElementById("selectBtn");
+const selectBtnEl = document.getElementById("select-btn");
 
-const colorSelectorEl = document.getElementById("colorSelector");
+const colorSelectorEl = document.getElementById("color-selector");
+const selectionMenuEl = document.getElementById('selection-menu');
+const colorSelectionForm = document.getElementById('color-selection-form');
 
+const colorColumnsSectionEl = document.getElementById('color-columns-section');
+const colorCodesSectionEl = document.getElementById('color-codes-section');
 
-selectBtnEl.addEventListener('submit', function (event) {
+selectBtnEl.addEventListener('click', function(event) {
     
 })
 
-/*
-fetch("https://www.thecolorapi.com/id{?hex,rgb,hsl,cmyk,format}", {
-    method: "GET",
-    headers: {"Content-Type":"application/json"}
+colorSelectionForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    colorColumnsSectionEl.innerHTML = "";
+    colorCodesSectionEl.innerHTML = "";
+    console.log(colorSelectorEl.value);
+
+    let hexcode = colorSelectorEl.value.replace('#', '');
+    console.log(hexcode);
+    let fetchPath = `https://www.thecolorapi.com/scheme?hex=${hexcode}&mode=${selectionMenuEl.value}`;
+    
+    fetch(`${fetchPath}`, {
+         method: "GET",
+         headers: {"Content-Type":"application/json"}
+     })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            console.log(data.colors[0].hex.value);
+            
+            for (let i = 0; i < (data.colors).length; i++) {
+                let indexColor = i;
+                console.log(indexColor);
+                
+                colorColumnsSectionEl.innerHTML += `
+                    <div class="color-column-display" id="color-column-${indexColor}"></div>
+                `
+                console.log(colorColumnsSectionEl);
+
+                colorCodesSectionEl.innerHTML += `
+                    <p>${data.colors[i].hex.value}</p>
+                `
+                
+                document.getElementById(`color-column-${indexColor}`).style.backgroundColor = data.colors[i].hex.value;
+            }    
+
+        })
+    
+    
 })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-*/
